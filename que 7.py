24262ab9e1943246f1e7361129d9620f7813c4fb
Suthar Kushalkumar <111581593+Suthar-Kushalkumar@users.noreply.github.com>
2022-11-09 15:55:41 +0530

    Add files via upload

---
+++ que 7.py
@@ -0,0 +1,39 @@
+import random
+def partitionSeq(a,low,high):
+    if(low >= high):
+        return
+    p = a[random.randint(low, high - 1)]
+    i = low - 1
+    j = high + 1
+    l=len(a)
+    while(l):
+        while(l):            
+            i += 1
+            if(a[i] >= p):
+                break
+        while(l):            
+            j -= 1
+            if(a[j] <= p):
+                break
+        if(i >= j):
+            break
+        a[i],a[j] = a[j],a[i]
+    partitionSeq(a, low, j)
+    partitionSeq(a, j+1, high)
+
+def quickSort(theSeq):
+    m = len( theSeq ) 
+    partitionSeq( theSeq, 0, m-1 )
+    
+arr=[]
+n=int(input("Number of element in array: "))
+for i in range(0,n):
+    print("Enter", i,"th element")
+    x=int(input())
+    arr=arr+[x]
+print(arr)
+print("Performing quick sort on array: ")
+quickSort(arr)
+print("Array after performing quick sort: ",arr) 
+
+
